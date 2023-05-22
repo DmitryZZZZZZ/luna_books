b897bc56335654ad59aacb09d3bb3a5a74608122
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-05-22 23:04:16 +0400

    update tests directory

---
+++ luna/tests/test_form.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from luna.forms import AddPostForm
+
+
+class PostFormTestCase(TestCase):
+    def test_valid_form_label(self):
+        form = AddPostForm()
+        self.assertEqual(form.fields['title'].label, 'Заголовок')
+
+    def test_empty_label(self):
+        form = AddPostForm()
+        self.assertEqual(form.fields['cat'].empty_label, 'Категория не выбрана')
---
+++ luna/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class LunaHomeViewTestCase(TestCase):
+    def test_url_main_page_status_code(self):
+        resp = self.client.get('')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_context_in_data_mixin(self):
+        resp = self.client.get('')
+        self.assertTrue('menu' in resp.context)
+        self.assertTrue('cats' in resp.context)
+
+    def test_correct_template(self):
+        resp = self.client.get('')
+        self.assertTemplateUsed(resp, 'luna/index.html')
